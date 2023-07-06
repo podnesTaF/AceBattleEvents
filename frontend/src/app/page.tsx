72a8f560c62d49270a8a2b51ca683ec77427daad
podnesTaF <podnestrade@gmail.com>
2023-07-06 10:50:04 +0100

    main page slider

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,56 +1,46 @@
 "use client";
 
+import IntroSlider from "@/components/main/IntroSlider";
 import CustomTitle from "@/components/shared/CustomTitle";
+import { useFetchEventsQuery } from "@/services/eventService";
 
 const textStyles =
   "text-lg sm:text-xl text-white border-r-2 border-red-500 px-2 sm:px-4 w-1/4 uppercase text-center";
 
 export default function Home() {
+  const { data, error, isLoading } = useFetchEventsQuery({
+    params: "",
+    currPage: 1,
+  });
+
   return (
     <>
-      <header className="w-full flex justify-center items-center h-96 h-calc-screen bg-[url('/main-intro-sm.jpg')] md:bg-[url('/main-intro.jpg')] bg-fixed bg-cover bg-no-repeat bg-top relative flex-col ">
-        <div className="absolute top-0 left-0 w-full h-full bg-[rgba(0,0,0,0.4)] z-0"></div>
-        <div className="sm:bg-[#1E1C1F] flex flex-col justify-center items-center sm:w-4/5 md:w-3/4 lg:w-3/5 xl:w-1/2 md:p-5 lg:p-10 h-80 z-10">
-          <h2 className="text-4xl text-white uppercase font-semibold flex-1 flex items-center">
-            ACE BATTLE EVENTS
-          </h2>
-          <div className="hidden sm:flex justify-center w-full p-4">
-            <p className={textStyles}>Buy</p>
-            <p className={textStyles}>Analyse</p>
-            <p className={textStyles}>Register</p>
-            <p className="text-lg sm:text-xl text-white px-2 sm:px-4 w-1/4 uppercase text-center">
-              Win
-            </p>
-          </div>
-        </div>
-        <div className="flex sm:hidden justify-center w-full py-4 px-2 bg-[#1E1C1F] z-10">
-          <p className={textStyles}>Buy</p>
-          <p className={textStyles}>Analyse</p>
-          <p className={textStyles}>Register</p>
-          <p className="text-lg sm:text-xl text-white px-2 sm:px-4 w-1/4 uppercase text-center">
-            Win
-          </p>
-        </div>
+      <header className="w-full flex justify-center items-center h-calc-screen bg-fixed bg-cover bg-no-repeat bg-top relative flex-col ">
+        {data && <IntroSlider events={data.events} />}
       </header>
       <main>
         <CustomTitle title={"BATTLE MILE"} subtitle=" RUNNING COMPETITIONS" />
-        <section className="py-[10%] max-w-4xl flex justify-center mx-auto">
+        <section className="py-[10%] max-w-4xl flex justify-center mx-auto gap-4 md:gap-10">
           <img
-            src="/auth-card.jpg"
+            src="/main-page-img.jpg"
             alt="card"
             className="object-cover object-top mr-6 w-1/2 max-h-[450px]"
           />
           <div className="flex flex-col justify-center w-1/2">
-            <h3 className="text-xl sm:text-2xl font-semibold">
+            <h3 className="text-2xl sm:text-4xl font-semibold">
               Welcome to the revolutionary world of Battle Mile.
             </h3>
-            <p className="mt-5 sm:mt-10">
+            <p className="text-xl mt-5 sm:mt-10">
               Running events have been transformed into epic battles between
-              teams. We are thrilled to present to you our cutting-edge web3
-              marketplace, designed exclusively for running club managers and
-              coaches.
+              teams.
             </p>
           </div>
+        </section>
+        <section className="w-full my-4 bg-[#1E1C1F] p-4">
+          <h3 className="text-white font-semibold text-2xl uppercase mb-4">
+            Teams
+          </h3>
+          <div></div>
         </section>
         <section className="w-full">
           <div className="w-full bg-[url('/main-sect-bg-sm.jpg')] md:bg-[url('/main-sect-bg.jpg')] bg-fixed bg-cover bg-top min-h-[380px] md:min-h-[480px] lg:min-h-[580px] relative flex items-end">
--- frontend/src/components/main/IntroSlide.tsx
+++ frontend/src/components/main/IntroSlide.tsx
@@ -1,45 +1,69 @@
-import { IEvent } from "@/models/IEvent";
+import Image from "next/image";
 import React from "react";
 
 interface Props {
-  event: IEvent;
+  event: any;
   isActive: boolean;
   idx: number;
   length: number;
+  setActiveSlide: Function;
+  slideProgress: number;
 }
 
-const IntroSlide: React.FC<Props> = ({ event, isActive, idx, length }) => {
+const IntroSlide: React.FC<Props> = ({
+  event,
+  isActive,
+  idx,
+  length,
+  setActiveSlide,
+  slideProgress,
+}) => {
   return (
-    <div
-      className={`${isActive ? "block" : "hidden"} w-full h-full bg-[url(${
-        event.introImage
-      })] bg-no-repeat bg-center bg-cover relative`}
-    >
-      <div className="absolute bottom-0 left-1/2 -translate-x-1/2 px-4 py-2 bg-[linear-gradient(360deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0) 100%)]">
-        <div className="flex justify-between w-full py-2 border-b-[1px] border-red-500 mb-4">
-          <h4 className="text-xl text-white uppercase font-semibold">
+    <div className={`${isActive ? "block" : "hidden"} w-full h-full relative`}>
+      <div className="absolute w-full md:w-3/4 lg:w-1/2 bottom-0 left-1/2 -translate-x-1/2 px-4 py-2 bg-gradient-to-b from-transparent to-black">
+        <div className="flex justify-between w-full py-2 border-b-[1px] border-red-500 mb-4 w">
+          <h4 className="text-xl sm:text-2xl text-white uppercase font-semibold">
             Team racing game
           </h4>
-          <h5 className="text-lg text-white uppercase font-semibold">
+          <h5 className="text-lg sm:text-xl text-white uppercase font-semibold">
             Competitions
           </h5>
         </div>
-        <h4 className="text-2xl text-white underline mb-2 md:mb-4">
+        <h4 className="text-2xl sm:text-4xl font-semibold text-white mb-2 md:mb-4">
           {event.title}
         </h4>
-        <h5 className="text-lg text-red-500 mb-4 md:mb-4">
+        <h5 className="text-lg sm:text-2xl text-red-500 mb-4 md:mb-4">
           {event.startDateTime}
         </h5>
-        {length > 1 &&
-          new Array(length).map((_, i) => (
-            <div
-              key={i}
-              className={`w-2/5 h-2 rounded-full ${
-                i === idx ? "bg-red-500" : "bg-white"
-              }`}
-            ></div>
-          ))}
+        <div className="w-full flex justify-around gap-4">
+          {length > 1 &&
+            Array.from({ length }).map((_, i) => (
+              <div
+                key={i}
+                className={`w-full h-[2px] rounded-full bg-white cursor-pointer`}
+                onClick={() => setActiveSlide(i)}
+              >
+                {i === idx && (
+                  <div
+                    className={`h-[2px] bg-red-500`}
+                    style={{
+                      width: `${slideProgress}%`,
+                    }}
+                  ></div>
+                )}
+              </div>
+            ))}
+        </div>
       </div>
+      <Image
+        src={
+          "https://storage.googleapis.com/abe_cloud_storage/image/large/dccfea3d-a639-4569-9c06-b37bbbb37130.jpg"
+        }
+        alt="bg"
+        className="object-cover xl:object-contain w-full h-full shadow-md"
+        width={1920}
+        height={980}
+      />
     </div>
   );
 };
