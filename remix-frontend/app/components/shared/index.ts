1af83201ad0bf9d72b3b3447d2b3ec990626df69
podnesTaF <podnestrade@gmail.com>
2023-09-04 16:30:43 +0200

    main page - new design - finish

--- remix-frontend/app/components/shared/index.ts
+++ remix-frontend/app/components/shared/index.ts
@@ -3,8 +3,18 @@ import CustomCarousel from "./CustomCarousel";
 import CustomTitle from "./CustomTitle";
 import Footer from "./Footer";
 import MessageDialog from "./MessageDialog";
+import SectionTitle from "./SectionTitle";
+import VideoItem from "./VideoItem";
 
 export * from "./forms";
 export * from "./header";
 export * from "./tables";
-export { ConfirmAlert, CustomCarousel, CustomTitle, Footer, MessageDialog };
+export {
+  ConfirmAlert,
+  CustomCarousel,
+  CustomTitle,
+  Footer,
+  MessageDialog,
+  SectionTitle,
+  VideoItem,
+};
