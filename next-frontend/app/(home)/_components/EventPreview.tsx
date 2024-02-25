c6021aaed5ac07431c47a67646dccfd7c2e7faf4
podnesTaF <podnestrade@gmail.com>
2024-02-25 09:12:32 +0100

    continue codding the main page

--- next-frontend/app/(home)/_components/EventPreview.tsx
+++ next-frontend/app/(home)/_components/EventPreview.tsx
@@ -1,9 +1,34 @@
 "use client";
 
+import { NewsCard } from "@/common/components/news/NewsCard";
 import CountDown from "@/common/components/ui/CountDown";
 import { Button } from "@/common/components/ui/button";
 import { ArrowUpRight } from "lucide-react";
 import Image from "next/image";
+
+const news = [
+  {
+    id: 1,
+    headImageSrc: "/images/paphos.jpg",
+    title: "New event in Paphos",
+    date: "2024-05-11T10:00:00",
+    description: "The next event will take place in Paphos, Cyprus",
+  },
+  {
+    id: 2,
+    headImageSrc: "/images/paphos.jpg",
+    title: "New event in Paphos",
+    date: "2024-05-11T10:00:00",
+    description: "The next event will take place in Paphos, Cyprus",
+  },
+  {
+    id: 3,
+    headImageSrc: "/images/paphos.jpg",
+    title: "New event in Paphos",
+    date: "2024-05-11T10:00:00",
+    description: "The next event will take place in Paphos, Cyprus",
+  },
+];
 
 const EventPreview = () => {
   return (
@@ -13,22 +38,22 @@ const EventPreview = () => {
         alt="event image"
         width={1600}
         height={800}
-        className="absolute left-0 top-0 w-full h-96 sm:h-[500px] object-cover lg:h-auto max-h-screen z-[2]"
+        className="absolute left-0 top-0 w-full h-[500px] sm:h-[600px] object-cover xl:h-auto max-h-screen z-[2]"
       />
-      <div className="p-6 sm:p-8 lg:p-20 pt-32 lg:pt-40 bg-main-section flex flex-col lg:flex-row-reverse min-h-screen gap-12 justify-between z-[5] relative ">
+      <div className="p-6 lg:p-20 pt-32 lg:pt-40 bg-main-section flex flex-col lg:flex-row-reverse min-h-screen gap-6 xl:gap-12 justify-between z-[5] relative ">
         <div className="flex flex-col flex-1 justify-between">
           <div className="flex w-full justify-center lg:justify-end mb-10 lg:mb-16">
             <CountDown date={"2024-05-11T10:00:00"} />
           </div>
           <div className="flex-[2] flex flex-col w-full max-w-lg ml-auto">
-            <h4 className="text-left w-full gradient-text-light text-2xl lg:text-4xl mb-4">
+            <h4 className="text-right w-full gradient-text-light text-2xl lg:text-4xl mb-4">
               Meet
             </h4>
-            <h2 className="text-4xl lg:text-5xl 2xl:text-6xl gradient-text-redish text-end">
+            <h2 className="text-4xl sm:text-5xl 2xl:text-6xl gradient-text-redish text-end">
               Ace Battle Mile <br /> Rating
             </h2>
           </div>
-          <div className="flex justify-end flex-1">
+          <div className="flex justify-start lg:justify-end flex-1">
             <Button
               size="lg"
               className="btn-clip pl-8 pr-12 py-6 rounded-none border-none"
@@ -45,11 +70,40 @@ const EventPreview = () => {
           <h4 className="text-white mb-4 hidden lg:block">Related news</h4>
           <div className="flex lg:flex-col gap-8 lg:gap-12">
             <div className="flex w-full gap-8">
-              <div className="flex w-72 xl:w-80 h-64 xl:h-72 bg-white/10"></div>
+              <div className="flex w-72 xl:w-80  2xl:w-96  bg-white/10">
+                {news.map(
+                  (item, index) =>
+                    index === 0 && (
+                      <NewsCard
+                        key={item.id}
+                        headImageSrc={item.headImageSrc}
+                        title={item.title}
+                        date={item.date}
+                        description={item.description}
+                        variant={"light"}
+                      />
+                    )
+                )}
+              </div>
             </div>
             <div className="flex w-full  gap-8 lg:gap-12">
-              <div className="flex w-72 xl:w-80 h-64 xl:h-72 bg-white/10"></div>
-              <div className="flex w-72 xl:w-80 h-64 xl:h-72 bg-white/10"></div>
+              {news.map(
+                (item, index) =>
+                  index !== 0 && (
+                    <div
+                      key={item.id}
+                      className="flex w-72 xl:w-80 2xl:w-96 bg-white/10"
+                    >
+                      <NewsCard
+                        headImageSrc={item.headImageSrc}
+                        title={item.title}
+                        date={item.date}
+                        description={item.description}
+                        variant={"light"}
+                      />
+                    </div>
+                  )
+              )}
             </div>
           </div>
         </div>
--- next-frontend/app/page.tsx
+++ next-frontend/app/page.tsx
@@ -1,6 +1,27 @@
 import { auth } from "@/app/(auth)/_lib/utils/auth";
 import Navbar from "@/common/components/navbar/Navbar";
+import Image from "next/image";
 import EventPreview from "./(home)/_components/EventPreview";
+
+const fascinatigSport = [
+  "Based on the most affordable, and trending human activity running.",
+  "Embraces running professionals along with youth generation and amateurs.",
+  "Entertaining all the way as the battle is continued till the last moment.",
+  "Meets demanding audience requests on more drama in less time.",
+];
+
+const entertainment = [
+  "Top musical artists.",
+  "Immersive activity areas and sponsors activities.",
+  "Something to get by all the audience tiers: from the general public and families to top level hospitality.",
+];
+
+const tech = [
+  "Innovative tracking and telemetry software.",
+  "Data driven analytics and forecasts.",
+  "All access broadcasts with state-of-art graphics.",
+  "Data security.",
+];
 
 export default async function Home() {
   const session = await auth();
@@ -12,6 +33,94 @@ export default async function Home() {
       </div>
       <main>
         <EventPreview />
+        <div className="relative bg-[url('/images/runners.jpg')] bg-fixed bg-center bg-cover bg-no-repeat w-full pt-64 lg:pt-96">
+          <div className="bg-primary flex flex-col justify-center items-center py-16 lg:py-24 px-12 gap-6 md:gap-12 text-white text-center">
+            <Image
+              src={"/logo/abm-logo.png"}
+              alt="abm"
+              width={250}
+              height={200}
+              className="w-40 h-auto lg:w-64"
+            />
+            <h2 className="text-4xl lg:text-5xl uppercase">
+              A team running game
+            </h2>
+            <p className="text-white text-xl lg:text-2xl max-w-lg">
+              Running events have been transformed into epic battles between
+              teams.
+            </p>
+          </div>
+        </div>
+        <div className="relative bg-[url('/images/stadium-lines.png')] bg-fixed bg-bottom bg-contain bg-no-repeat w-full pt-64 lg:pt-96">
+          <div className="flex flex-col lg:flex-row w-full">
+            <div className="bg-[#EE342C] pt-4 flex-1 lg:pt-6 clip-hep  z-10">
+              <h2 className="text-4xl xl:text-6xl pl-4 lg:pl-6 text-primary uppercase">
+                Full scale event
+              </h2>
+              <div className="py-20 mx-auto lg:mx-0 lg:pl-10 text-white w-full lg:w-[70%]">
+                <h3 className="text-3xl lg:text-4xl uppercase mb-20">
+                  Fascinating sport
+                </h3>
+                <ul className="list-none flex flex-col gap-4">
+                  {fascinatigSport.map((feature, index) => (
+                    <li key={index} className="flex gap-2">
+                      <Image
+                        src={"/icons/point.svg"}
+                        width={20}
+                        height={20}
+                        className="w-4 h-4 lg:w-6 lg:h-6"
+                        alt="tick"
+                      />
+                      <p className=" text-lg xl:text-xl">{feature}</p>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className="bg-white flex-1 relative">
+              <div className="py-20 mx-auto lg:ml-20 text-black w-full">
+                <h3 className="text-3xl lg:text-4xl uppercase mb-20">
+                  entertainment
+                </h3>
+                <ul className="list-none flex flex-col gap-4">
+                  {entertainment.map((feature, index) => (
+                    <li key={index} className="flex gap-2">
+                      <Image
+                        src={"/icons/point.svg"}
+                        width={20}
+                        height={20}
+                        className="w-4 h-4 lg:w-6 lg:h-6"
+                        alt="tick"
+                      />
+                      <p className=" text-lg xl:text-xl">{feature}</p>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className="bg-gray-300 flex-1 relative ">
+              <div className="py-20 mx-auto lg:ml-20 text-black w-full">
+                <h3 className="text-3xl lg:text-4xl uppercase mb-20">
+                  TECHNOLOGY
+                </h3>
+                <ul className="list-none flex flex-col gap-4">
+                  {tech.map((feature, index) => (
+                    <li key={index} className="flex gap-2">
+                      <Image
+                        src={"/icons/point.svg"}
+                        width={20}
+                        height={20}
+                        className="w-4 h-4 lg:w-6 lg:h-6"
+                        alt="tick"
+                      />
+                      <p className=" text-lg xl:text-xl">{feature}</p>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
     </>
   );
